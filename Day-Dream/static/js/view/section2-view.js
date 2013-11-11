d72b963d8f97d78aae63298302c9a2bd6ce15b1b
CloudSun <violetcloudy@hotmail.com>
2013-11-11 02:12:15 -0800

    add Nancy Rotate 3D Cube

--- Day-Dream/static/js/view/section2-view.js
+++ Day-Dream/static/js/view/section2-view.js
@@ -35,6 +35,45 @@
         //TODO
         console.log(name + "View addEvent");
 
+        for (var i = 0; i < $(".cubebox").length; i++) {
+            var t = $($(".cubebox")[i]);
+            t.attr("index", i);
+            var clickTime = 0
+            t.unbind("click").click(function () {
+                var font = $($(this).find(".box-font"));
+                var back = $($(this).find(".box-end"));
+                switch (parseInt($(this).attr("index")) % 3) {
+                    case 0:
+                        $(this).addClass("box_x_transition");
+                        break;
+                    case 1:
+                        $(this).addClass("box_y_transition");
+                        break;
+                    case 2:
+                        $(this).addClass("box_z_transition");
+                        break;
+                }
+            });
+        }
+        /*
+        $("#box").click(function () {
+            $("#box1_1").addClass("box_x_transition");
+        });
+        $("#boxY").click(function () {
+            $("#boxY").addClass("box_y_transition");
+        });
+        $("#boxZ").click(function () {
+            $("#boxZ").addClass("box_z_transition");
+        });
+        $("#back").click(function () {
+
+        })
+        */
+        /*$("#box").on("webkitTransitionEnd", function () {
+            $("#box").removeClass("box_x_transition");
+            $("#box").addClass("box_y_transition");
+        })*/
+
         CallbackL(arguments);
     };
 
