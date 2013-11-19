c274c94000f75f2ed9710651ab95531ef6b9577c
CloudSun <violetcloudy@hotmail.com>
2013-11-19 10:32:45 +0800

    Update rotate3Dpane.js

--- Day-Dream/static/js/custom/rotate3Dpane.js
+++ Day-Dream/static/js/custom/rotate3Dpane.js
@@ -1,38 +1,67 @@
-﻿var Rotate3DCube = {
+var Rotate3DCube = {
     container: {
         target: $("#Rotate3DCubeContainer"),
-        width: $("#Rotate3DCubeContainer").width(),
-        height: $("#Rotate3DCubeContainer").height(),
     },
     cube: {
         size: {
-            width: 100,
-            height: 100,
+            w: 70,
+            h: 70,
         },
         row: 0,
         col: 0,
     },
-    Init: function () {
+    Init: function (target,next) {
+        console.log("Init 3D pane");
         //clear #Rotate3DCubeContainer
         var _this = this;
         _this.container.target.html("");
         var sizeScale = Resize.MapCubeContainer(_this.container.target)
         _this.row = sizeScale.scaleHeight;
         _this.col = sizeScale.scaleWidth;
-        //_this.cube.row = _this.container.width/
-        if ($("#Rotate3DCubeContainer").children().length==0){
+        if (_this.container.target.children().length==0){
             for (var r = 0; r < _this.row; r++) {
-                for (var d = 0; d < this.col; d++) {
+                for (var d = 0; d < _this.col; d++) {
                     var cube = $('<div class="cubepane"></div>')
-                    var cubeW = _this.container.width/_this.row;
-                    var cubeH = _this.container.height/_this.col;
                     cube.css({
-                        "width": cubeW+"px",
-                        "height": cubeH + "px",
+                        "width":_this.cube.size.w+"px",
+                        "height": _this.cube.size.h + "px",
                     });
+                    //init front pane
+                    var front = $('<div class="pane-front"></div>');
+                    //init back pane
+                    var back = $('<div class="pane-end"></div>')
+
+                    //add front content
+                    var frontContent = target.clone();
+                    
+                    //add back content
+                    next = Resize.ImageActualCenter(next);
+                    var backContent = next.clone();
+
+                    //css posiion
+                    var cube_left = d * _this.cube.size.w;
+                    var cube_top = r * _this.cube.size.h;
+                    frontContent.css({
+                        "left": target.currentPosition.left - cube_left + "px",
+                        "top": target.currentPosition.top - cube_top + "px",
+                        "margin": "0px",
+                        "display":"block",
+                    });
+                    backContent.css({
+                        "left": next.currentPosition.left - cube_left + "px",
+                        "top": target.currentPosition.top - cube_top + "px",
+                        "margin": "0px",
+                        "display": "block",
+                    })
+
+                    front.append(frontContent);
+                    back.append(backContent);
+
                     //add front pane
-                    cube.append('<div class="pane-front">正</div>');
-                    cube.append('<div class="pane-end">正</div>');
+                    cube.append(front);
+                    //add back pane
+                    cube.append(back);
+
                     cube.attr("id", "cube-pane-" + r + "-" + d);
                     this.container.target.append(cube);
                 }
@@ -43,6 +72,7 @@
                 t.attr("index", i);
             }
 
+            _this.container.target.addClass("top-pane");
             this.AddEvent();
         }
     },
@@ -52,23 +82,29 @@
         this.container.target.click(function () {
             //add delay
             var delay = 0.1;
-            var duration = 0.5;
+            var duration = 0.4;
             var panes = $(_this.container.target.children());
             for (var i = 0; i < panes.length; i++) {
                 var pane = $(panes[i]);
+                var backpane = pane.find(".pane-end");
                 //add transition class
                 var r = parseInt(i / _this.col);
-                var d = i % _this.row;
+                var d = i % _this.col;
                 var r_time = 0;
                 r > 0 && (r_time = r * _this.col * delay);
                 var d_time = 0;
-                if (r % 2 == 0) {
+                //backpane add ready ckass
+                
+                /*if (r % 2 == 0) {
+                    backpane.addClass("backpane-ready-to-right");
                     d_time = d * delay;
                     pane.addClass("pane-to-right");
-                } else {
+                /*} else {
+                   */
+                    backpane.addClass("backpane-ready-to-left");
                     d_time = (_this.col - 1 - d) * delay;
                     pane.addClass("pane-to-left");
-                }
+                /*}*/
                 var spaceTime = r_time + d_time;
                 pane.css("-webkit-transition-delay", spaceTime + "s");
                 pane.css("-webkit-transition-duration", duration + "s");
